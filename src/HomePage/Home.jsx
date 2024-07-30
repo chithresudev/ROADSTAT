c8d78426604ebbdb06acb70d07fafb39267c146a
rupinder-maan <168559860+rupinder-maan@users.noreply.github.com>
2024-07-30 14:23:03 -0400

    Update Home.jsx

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import MapComponent from './MapComponent';
-import './Home.css';
+import './home.css';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
