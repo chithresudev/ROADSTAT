40ede73fc4ab6c7c77b8d4586cc68aa64fe89099
Amnipriya Sowrirajan <146047412+amnipriyasowrirajan@users.noreply.github.com>
2024-11-25 13:07:03 -0500

    Update LoginPage.jsx

--- Frontend/src/LoginPage/LoginPage.jsx
+++ Frontend/src/LoginPage/LoginPage.jsx
@@ -1,135 +1,112 @@
-import React, { useState, useEffect } from "react";
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import { useNavigate, Link } from "react-router-dom";
-import { authenticationService } from "@/_services";
-import "./loginPage.css";
-import { FaEye, FaEyeSlash } from "react-icons/fa"; // Importing eye icons
+import { useEffect } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate, Link } from 'react-router-dom';
+import { useAuth } from '../context/authContext';
+//import { authenticationService } from '../../../Backend/src/_services';
+import './loginPage.css';
 
 function LoginPage() {
-  const navigate = useNavigate();
-  const [initialValues, setInitialValues] = useState({
-    username: "",
-    password: "",
-  });
+    const navigate = useNavigate();
+    const { user, login, message, clearMessage } = useAuth();
+    const initialValues = {
+        identifier: '', // Can be username or email
+        password: ''
+    };
 
-  const [passwordVisible, setPasswordVisible] = useState(false); // New state for password visibility
+    useEffect(() => {
+        if (user) {
+            navigate('/');
+        }
+    }, [user, navigate]);
 
-  useEffect(() => {
-    if (authenticationService.currentUserValue) {
-      navigate("/");
-    }
-  }, [navigate]);
+    useEffect(() => {
+        if (message.text) {
+            // Optionally show or handle message text
+            clearMessage();
+        }
+    }, [message, clearMessage]);
 
-  const onSubmit = ({ username, password }, { setStatus, setSubmitting }) => {
-    setStatus();
-    authenticationService.login(username, password).then(
-      (user) => {
-        const { from } = navigate.location?.state?.from || {
-          from: { pathname: "/" },
-        };
-        navigate(from);
-      },
-      (error) => {
-        setSubmitting(false);
-        setStatus(error);
-      }
-    );
-  };
+    const onSubmit = async ({ identifier, password }, { setStatus, setSubmitting }) => {
+        setStatus(null);
+        try{
+            const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(identifier);
 
-  // Function to toggle password visibility
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
+            // Dynamically construct payload
+            const payload = {
+                ...(isEmail ? { email: identifier } : { username: identifier }),
+                password
+            };
 
-  return (
-    <div className="login_bg">
-      <div className="logos">
-        <img
-          src="/images/companyLogo.png"
-          alt="Logo"
-          className="company_logo"
-        />
-        <img src="/images/roadstatLogo.png" alt="Logo" className="road_logo" />
-      </div>
-      <div className="half_login">
-        <div className="login_page">
-          <h2 className="login">Login</h2>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={Yup.object().shape({
-              username: Yup.string().required("Username is required"),
-              password: Yup.string().required("Password is required"),
-            })}
-            onSubmit={onSubmit}
-          >
-            {({ errors, status, touched, isSubmitting }) => (
-              <Form>
-                <div className="login_forms">
-                  <Field
-                    type="text"
-                    name="username"
-                    className={
-                      "text_bar" +
-                      (errors.username && touched.username ? " is-invalid" : "")
-                    }
-                    placeholder="Username"
-                  />
-                  <div className="error_message">
-                    <ErrorMessage name="username" />
-                  </div>
-                </div>
+            await login(payload);
+            const { from } = navigate.location?.state?.from || { from: { pathname: "/" } };
+            navigate(from);
+        }
+        catch(error) {
+            setSubmitting(false);
+            setStatus(error);
+        }
+    };
 
-                <div className="login_forms">
-                  <div className="passwordInputContainer">
-                    <Field
-                      type={passwordVisible ? "text" : "password"} // Toggle type based on visibility
-                      name="password"
-                      className={
-                        "text_bar" +
-                        (errors.password && touched.password
-                          ? " is-invalid"
-                          : "")
-                      }
-                      placeholder="Password"
-                    />
-                    <span
-                      className="passwordToggleIcon"
-                      onClick={togglePasswordVisibility}
-                    >
-                      {passwordVisible ? <FaEyeSlash /> : <FaEye />}
-                    </span>
-                  </div>
-                  <div className="error_message">
-                    <ErrorMessage name="password" />
-                  </div>
-                </div>
+    return (
+        <div className='login_bg'>
+            <div>
+                <img src="/images/companyLogo.png" alt="Logo" className="company_logo" />
+                <img src="/images/roadstatNewLogo.png" alt="Logo" className="road_logo" />
+            </div>
+            <div className='login_page'>
+                <h2 className='login'>Login</h2>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={Yup.object().shape({
+                        identifier: Yup.string().required('Username or Email is required'),
+                        password: Yup.string().required('Password is required')
+                    })}
+                    onSubmit={onSubmit}>
 
-                <div>
-                  <button
-                    type="submit"
-                    className="login_btn"
-                    disabled={isSubmitting}
-                  >
-                    Login
-                  </button>
-                  {isSubmitting && (
-                    <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                  )}
-                </div>
-                {status && <div className="alert_danger">{status}</div>}
-                <div>
-                  <p className="signup">
-                    Don't have an account? <Link to="/signup">Sign up</Link>
-                  </p>
-                </div>
-              </Form>
-            )}
-          </Formik>
+                    {({ errors, status, touched, isSubmitting }) => (
+                        <Form>
+                            <div>
+                                <Field 
+                                    type="text" 
+                                    name="identifier" 
+                                    className={'text_bar' + (errors.identifier && touched.identifier ? ' is-invalid' : '')} 
+                                    placeholder="Username or Email"
+                                />
+                                <div className="error_message">
+                                    <ErrorMessage name="identifier" />
+                                </div>
+                            </div>
+
+                            <Field 
+                                type='password' 
+                                name="password" 
+                                className={'text_bar' + (errors.password && touched.password ? ' is-invalid' : '')} 
+                                placeholder="Password"
+                            />
+                            <div className="error_message">
+                                <ErrorMessage name="password" />
+                            </div>
+                            <div>
+                                <button type="submit" className="login_btn" disabled={isSubmitting}>Login</button>
+                                {isSubmitting &&
+                                    <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                                }
+                            </div>
+                            {status &&
+                                <div className="alert_danger">{status}</div>
+                            }
+                            <div>
+                                <p className='signup'>Don't have an account? <Link to="/signup">Sign up</Link></p>
+                            </div>
+
+                        </Form>
+                    )}
+
+                </Formik>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
-export { LoginPage };
+export default LoginPage;
